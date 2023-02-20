1044e0abd8c5af766ae4006e40b9df141630efa6
Yeaeun Han <yenhan.dev@gmail.com>
2023-02-19 20:45:08 -0500

    feat: make shadow lighter

--- src/Experience/Objects/Environment.js
+++ src/Experience/Objects/Environment.js
@@ -11,13 +11,16 @@ export default class Environment {
     this.setSunlight();
   }
   setSunlight() {
-    this.sunLight = new THREE.DirectionalLight(0xffffff, 3);
+    this.sunLight = new THREE.DirectionalLight("#ffffff", 3);
     this.sunLight.castShadow = true;
     this.sunLight.shadow.camera.far = 20;
     this.sunLight.shadow.mapSize.set(1024, 1024);
     this.sunLight.shadow.normalBias = 0.05;
     this.sunLight.position.set(1.5, 7, 3);
     this.scene.add(this.sunLight);
+
+    const ambientLight = new THREE.AmbientLight("#ffffff", 1);
+    this.scene.add(ambientLight);
   }
 
   resize() {}
